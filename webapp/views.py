6c9b1431f9351c326ffc9557d8cd87b0247174df
bijanghanei <iambijanghanei@gmail.com>
2023-12-30 13:33:18 -0500

    login added

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #Authenticate
+        user = authenticate(request,username= username, password = password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"You have been successfuly logged in.")
+            return redirect('home')
+        else:
+            messages.success(request,"There is something wrong , please try again or make sure you are registered.")
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
+
+def logout_user(request):
+    pass
