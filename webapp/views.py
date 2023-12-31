fda9ec0bc03071b14a61384afa95553af04005dc
bijanghanei <iambijanghanei@gmail.com>
2023-12-31 02:15:19 -0500

    Authentication completed

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,logout,authenticate
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
     if request.method == 'POST':
@@ -22,3 +23,20 @@ def logout_user(request):
     logout(request)
     messages.success(request,"You are logged out, hope to see you soon.")
     return redirect('home')
+
+def register_user(request):
+    if request.method=='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authemticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request,username=username,password=password)
+            login(request,user)
+            messages.success(request,'You have successfuly registered')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request,'register.html',{'form':form})
+    return render(request,'register.html',{'form':form})
