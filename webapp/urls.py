46a84669939458e488206130e8f83f435d12cc1c
bijanghanei <iambijanghanei@gmail.com>
2023-12-31 05:52:01 -0500

    delete feature added

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('logout/',views.logout_user,name='logout'),
-    path('register_user/',views.register_user,name='register')
+    path('register_user/',views.register_user,name='register'),
+    path('customer_record/<int:pk>',views.customer_record,name = 'customer_record'),
+    path('delete_record/<int:pk>',views.delete_record,name='delete_record')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import login,logout,authenticate
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -17,7 +19,7 @@ def home(request):
             messages.success(request,"There is something wrong , please try again or make sure you are registered.")
             return redirect('home')
     else:
-        return render(request,'home.html',{})
+        return render(request,'home.html',{'records':records})
 
 def logout_user(request):
     logout(request)
@@ -40,3 +42,23 @@ def register_user(request):
         form = SignUpForm()
         return render(request,'register.html',{'form':form})
     return render(request,'register.html',{'form':form})
+
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request,'record.html',{'customer_record':customer_record})
+    else:
+        messages.success(request,'You need to login first')
+        return redirect('home')
+    
+
+def delete_record(request,pk):
+    if request.user.is_authenticated:
+        delete_item = Record.objects.get(id=pk)
+        delete_item.delete()
+        messages.success(request,'The record has been deleted sucessfuly')
+        return redirect('home')
+    else:
+        messages.success(request,'You should login first')
+        return redirect('home')
